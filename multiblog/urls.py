60621a5f08a10cb3484f0c29619187a2e9e23063
VertigoStr <VertigoStr@github.com>
2016-07-14 13:03:56 +0400

    edit templates

--- multiblog/urls.py
+++ multiblog/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
 	url(r'^my_profile/(?P<pk>[0-9]+)/$', views.my_profile, name='my_profile'),
 	url(r'^new', views.new_publication, name='new'),
 	url(r'^publication/(?P<pk>[0-9]+)/$', views.full_publication, name='full'),
+	url(r'^publication/(?P<pk>[0-9]+)/edit/$', views.edit_publication, name='edit'),
+	url(r'^logout/$', 'django.contrib.auth.views.logout', {'next_page': '/'}),
 ]
--- multiblog/views.py
+++ multiblog/views.py
@@ -5,31 +5,36 @@ from django.contrib import auth
 import datetime
 
 def main(request):
-	form = UserAuthForm()
+	if request.GET.get('new'):
+		return HttpResponseRedirect("/new/")
+
 	if request.GET.get('logout'):
 		auth.logout(request)
-		return HttpResponseRedirect("")
+		return HttpResponseRedirect(request.path)
 
 	if request.method == "POST":
+		error = 'Неверный логин или пароль'
 		email = request.POST['email']
 		password = request.POST['password']
 
 		if 'submit-register' in request.POST:
 			print('submit-register')
-			new_user = Blogger.objects.create_user(email, password)
+			try:
+				new_user = Blogger.objects.create_user(email, password)
+			except Exception:
+				error = 'Данный Email-адрес уже используется'
 
 		new_user = auth.authenticate(email=email, password=password)
-		
+
 		if new_user:
 			auth.login(request, new_user)	
-
-		return HttpResponseRedirect("/my_profile/" + str(auth.get_user(request).id))
-
-
+			return HttpResponseRedirect("/my_profile/" + str(auth.get_user(request).id))
+		else:
+			return {'error': error}
 
 	publ = Publication.objects.all().order_by("id")
 
-	return render(request, 'multiblog/main.html', {'form':form, 'user': auth.get_user(request), 'publ':publ})
+	return render(request, 'multiblog/main.html', {'user': auth.get_user(request), 'publ':publ})
 
 def my_profile(request, pk):
 	blogger = Blogger.objects.get(id=pk)
@@ -48,11 +53,29 @@ def new_publication(request):
 			post.time = datetime.datetime.now()
 			post.author = auth.get_user(request)
 			post.save()
-			return HttpResponseRedirect('')
+			return HttpResponseRedirect('/')
 	else:
 		form = PublicationForm()
 
 	return render(request, 'multiblog/new.html', {'form':form, 'user': auth.get_user(request),})		
+
+def edit_publication(request, pk):
+	post = get_object_or_404(Publication, pk=pk)
+
+	if request.method == "POST":
+		form = PublicationForm(request.POST, instance=post)
+		if form.is_valid():
+			post = form.save(commit=False)
+			post.title =  form.cleaned_data['title']
+			post.abstract = form.cleaned_data['abstract']
+			post.full_text =  form.cleaned_data['full_text']
+			post.time = datetime.datetime.now()
+			post.author = auth.get_user(request)
+			post.save()
+			return HttpResponseRedirect("/publication/" + str(pk))
+	else:
+		form = PublicationForm(instance=post)
+	return render(request, 'multiblog/new.html', {'form':form})
 
 def full_publication(request, pk):
 	post = get_object_or_404(Publication, pk=pk)
